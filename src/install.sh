3833b989d3ae26074c3524a323724b5ea3d189b7
pratas <pratas@ua.pt>
2016-05-16 10:50:37 +0100

    instsll

--- src/install.sh
+++ src/install.sh
@@ -7,6 +7,7 @@
 ###############################################################################
 # SYSTEM FLAGS ================================================================
 INSTALL_DEPENDENCIES=1;
+  INSTALL_LIBCURL=1;
   INSTALL_ZLIB=1;
   INSTALL_CMAKE=1;
   INSTALL_LIBBOOST=1;
@@ -18,6 +19,7 @@ INSTALL_GECO=1;
 INSTALL_GREEN=1;
 INSTALL_COGI=1;
 INSTALL_GDC2=1;
+INSTALL_DNAZIP=0; # ERROR ON COMPILATION
 INSTALL_FRESCO=1;
 INSTALL_ORCOM=1;
 INSTALL_DSRC=1;
@@ -26,7 +28,8 @@ INSTALL_FQC=1;
 INSTALL_FQZCOMP=1;
 INSTALL_QUIP=1;
 INSTALL_GRS=1;
-INSTALL_NGC=1;
+INSTALL_NGC=0; # JAVA & ...
+INSTALL_ABRC=0; # MISSING FOLLOWING INSTRUCTIONS
 INSTALL_DEEZ=1;
 INSTALL_SCALCE=1;
 INSTALL_LWFQZIP=1;
@@ -46,6 +49,11 @@ cd progs/
 ###############################################################################
 # GET LIBS ====================================================================
 if [[ "$INSTALL_DEPENDENCIES" -eq "1" ]]; then
+  # GET LIBCURL ===============================================================
+  if [[ "$INSTALL_LIBCURL" -eq "1" ]]; then
+  sudo apt-get install libcurl4-nss-dev
+  sudo apt-get install libcurl-dev
+  fi
   # GET ZLIB ==================================================================
   if [[ "$INSTALL_ZLIB" -eq "1" ]]; then
   rm -f zlib_1.2.8.dfsg.orig.tar.gz
@@ -97,6 +105,8 @@ gcc -o ../iDoComp.run idc_generate_mapping.c main.c stats.c arith.c \
 fasta_decompressor.c idc_load_chr.c os_stream.c fasta_compressor.c \
 sam_stream.c -lm
 cd ../../../
+mv iDoComp_website_v1 idocomp
+rm -f iDoComp_website_v1.tar.gz
 fi
 ###############################################################################
 # GET DNACompact ==============================================================
@@ -105,10 +115,12 @@ COMPACTPATH="projects/dnacompact/files/latest";
 rm -f FILES
 wget http://sourceforge.net/$COMPACTPATH/download?source=typ_redirect -O FILES
 unzip FILES
-cd Latest\ Public\ source\ code
+mv Latest\ Public\ source\ code dnacompact
+cd dnacompact/
 make; 
-cp compact ../
+#cp compact ../
 cd ..
+rm -f FILES
 ulimit -s 200000000 # UNLIMIT SIZE OF OPEN FILES
 #(time ./compact n ../BI/B9-CLEAN ) &> REPORT_B9_CLEAN ;
 fi
@@ -121,9 +133,10 @@ unzip cogi.zip
 cd cogi/
 mkdir bin/
 make
-cp bin/compress ../cogi-compress
-cp bin/uncompress ../cogi-uncompress
+cp bin/compress cogi-compress
+cp bin/uncompress cogi-uncompress
 cd ../
+rm -f cogi.zip
 fi
 ###############################################################################
 # GET GECO ====================================================================
@@ -133,8 +146,8 @@ git clone https://github.com/pratas/geco.git
 cd geco/src/
 cmake .
 make
-cp GeCo ../../
-cp GeDe ../../
+cp GeCo ../
+cp GeDe ../
 cd ../../
 fi
 ###############################################################################
@@ -143,11 +156,13 @@ if [[ "$INSTALL_GREEN" -eq "1" ]]; then
 rm -f GReEn1.tar.gz;
 wget http://bioinformatics.ua.pt/wp-content/uploads/2014/09/GReEn1.tar.gz
 tar -xvzf GReEn1.tar.gz
-cd GReEn1/
+mv GReEn1/ green/
+cd green/
 make
-cp GReEnC ../
-cp GReEnD ../
+#cp GReEnC ../
+#cp GReEnD ../
 cd ..
+rm -f GReEn1.tar.gz
 fi
 ###############################################################################
 # GET FRESCO ==================================================================
@@ -158,9 +173,8 @@ cd FRESCO/build/
 printf 'CPP=g++\nCC=gcc\nLD=g++\nLIB=ar\nWINDRES=\nOBJS_RELEASE=../main.o\n\nRelease: ../FRESCO\n../FRESCO: $(OBJS_RELEASE)\n\t@echo Building console executable ../FRESCO\n\t@g++ -lz -lboost_system -lboost_filesystem -lboost_iostreams -o ../FRESCO $(OBJS_RELEASE) -s -lz -lboost_system -lboost_filesystem -lboost_iostreams\n\n../main.o: ../source/main.cpp\n\t@echo Compiling: ../source/main.cpp\n\t@g++ -Wall -Wunused-variable -fexceptions -std=c++11 -O3 -c ../source/main.cpp -o ../main.o\n\n../source/main.cpp: ../include/* ../include/Sequence/* ../include/Index/*.h ../include/Compressor/* ../include/Serializer/* ../include/Config/ConfigFileParser.h\n\n.PHONY: clean\n\nclean:\n\t@echo Delete $(OBJS_RELEASE) ../FRESCO\n\t-@rm -f $(OBJS_RELEASE) ../FRESCO\n' > Makefile;
 make clean ;
 make ; 
-cd ../
-cp FRESCO ../FRESCO-BIN
-cd ../
+cd ../../
+mv FRESCO fresco
 fi
 ###############################################################################
 # GET GDC =====================================================================
@@ -172,8 +186,9 @@ cd gdc2/gdc_2/Gdc2/
 # LIBRARIES ORDER ACCESS CREATE SOME PROBLES (WE ADD THEM TO THE END)
 printf '\nall: gdc2 \n\nCC      = g++\nCFLAGS  = -Wall -O3 -m64 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -std=c++11 \nCLINK   = -lm -O3 -m64 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -std=c++11 -lz \n\n.cpp.o: \n\t$(CC) $(CFLAGS) -c $< -o $@ \n\ngdc2: c1stage.o c2stage.o fasta.o hasher.o main.o p1stage.o qsmodel.o queue.o rangecod.o timer.o \n\t$(CC) $(CLINK) -o gdc2 c1stage.o c2stage.o fasta.o hasher.o main.o p1stage.o qsmodel.o queue.o rangecod.o timer.o ../libs/libaelf64.a -lz -lpthread \n\nclean: \n\trm gdc2 \n\trm *.o \n' > Makefile;
 make clean ; make
-cp gdc2 ../../../GDC2 # TO NOT OVERLAP FOLDER NAME
+cp gdc2 ../../GDC2 # TO NOT OVERLAP FOLDER NAME
 cd ../../../
+rm -f gdc2.tar.gz
 fi
 ###############################################################################
 # DNAzip ======================================================================
@@ -192,15 +207,17 @@ if [[ "$INSTALL_ERGC" -eq "1" ]]; then
 rm -f ERGC.zip
 wget http://engr.uconn.edu/~rajasek/ERGC.zip
 unzip ERGC.zip
-cd ERGC/
+mv ERGC ergc
+cd ergc/
 make
 chmod 777 7za 7z
-cp *.class ..
-cp 7za ..
-cp 7z ..
-cp SCRIPT_ERGC_COMP ..
-cp SCRIPT_ERGC_DECOMP ..
+#cp *.class ..
+#cp 7za ..
+#cp 7z ..
+#cp SCRIPT_ERGC_COMP ..
+#cp SCRIPT_ERGC_DECOMP ..
 cd ..
+rm -f ERGC.zip
 fi
 ###############################################################################
 # GET ORCOM ===================================================================
@@ -209,8 +226,8 @@ rm -fr orcom/
 git clone https://github.com/lrog/orcom.git
 cd orcom/
 make
-cp bin/orcom_bin ../
-cp bin/orcom_pack ../
+cp bin/orcom_bin .
+cp bin/orcom_pack .
 cd ..
 fi
 ###############################################################################
@@ -220,7 +237,7 @@ rm -fr dsrc/
 git clone https://github.com/lrog/dsrc.git
 cd dsrc/
 make
-cp bin/dsrc ../
+cp bin/dsrc .
 cd ..
 fi
 ###############################################################################
@@ -229,12 +246,14 @@ if [[ "$INSTALL_MFCOMPRESS" -eq "1" ]]; then
 rm -f MFCompress-src-1.01.tgz;
 wget http://sweet.ua.pt/ap/software/mfcompress/MFCompress-src-1.01.tgz
 tar -xzf MFCompress-src-1.01.tgz
-cd MFCompress-src-1.01/
+mv MFCompress-src-1.01/ mfcompress
+cd mfcompress/
 cp Makefile.linux Makefile # make -f Makefile.linux
 make
-cp MFCompressC ..
-cp MFCompressD ..
+#cp MFCompressC ..
+#cp MFCompressD ..
 cd ..
+rm -f MFCompress-src-1.01.tgz;
 fi
 ###############################################################################
 # GET FQC =====================================================================
@@ -243,12 +262,14 @@ rm -f FQC_LINUX_64bit.tar.gz
 wget \
 http://metagenomics.atc.tcs.com/Compression_archive/FQC/FQC_LINUX_64bit.tar.gz
 tar -xzf FQC_LINUX_64bit.tar.gz
-cd FQC_LINUX_64bit/
-cp 7za ../
-cp fcompfastq ../
-cp fdecompfastq ../
-cp fqc ../
+mv FQC_LINUX_64bit/ fqc/
+cd fqc/
+#cp 7za ../
+#cp fcompfastq ../
+#cp fdecompfastq ../
+#cp fqc ../
 cd ../
+rm -f FQC_LINUX_64bit.tar.gz
 fi
 ###############################################################################
 # GET FQZ_COMP ================================================================
@@ -256,40 +277,47 @@ if [[ "$INSTALL_FQZCOMP" -eq "1" ]]; then
 rm -f fqzcomp-4.6.tar.gz
 wget https://downloads.sourceforge.net/project/fqzcomp/fqzcomp-4.6.tar.gz
 tar -xzf fqzcomp-4.6.tar.gz
-cd fqzcomp-4.6/
+mv fqzcomp-4.6/ fqzcomp/
+cd fqzcomp/
 make
-cp fqz_comp ../
+#cp fqz_comp ../
 cd ..
+rm -f fqzcomp-4.6.tar.gz
 fi
 ###############################################################################
 # GET QUIP ====================================================================
 if [[ "$INSTALL_QUIP" -eq "1" ]]; then
+rm -f quip-1.1.8.tar.gz
 wget http://homes.cs.washington.edu/~dcjones/quip/quip-1.1.8.tar.gz
 tar -xzf quip-1.1.8.tar.gz
-cd quip-1.1.8/
+mv quip-1.1.8/ quip/
+cd quip/
 ./configure
 cd src/
 make
-cp quip ../../
+cp quip ../
 cd ../../
+rm -f quip-1.1.8.tar.gz
 fi
 
 ###############################################################################
 # GET GRS =====================================================================
 if [[ "$INSTALL_GRS" -eq "1" ]]; then
 rm -f GRS1.0_Source_Code.tar.bz2
-http://gmdd.shgmo.org/Computational-Biology/GRS/GRS1.0_Source_Code.tar.bz2
+wget http://gmdd.shgmo.org/Computational-Biology/GRS/GRS1.0_Source_Code.tar.bz2
 tar -xjf GRS1.0_Source_Code.tar.bz2
-cd GRS1.0_Source_Code/
+mv GRS1.0_Source_Code/ grs/
+cd grs/
 . install.sh
-cd ..
+cd ../../../
+rm -f GRS1.0_Source_Code.tar.bz2
 fi
 ###############################################################################
 # GET NGC =====================================================================
 if [[ "$INSTALL_NGC" -eq "1" ]]; then
-rm -fr NGC
-mkdir NGC
-cd NGC
+rm -fr ngc
+mkdir ngc
+cd ngc
 wget http://www.cibiv.at/~niko/ngc/download/ngc-core-0.0.1-standalone.jar
 wget http://www.cibiv.at/~niko/ngc/download/ngc-core-0.0.1.jar
 cd ../
@@ -306,11 +334,14 @@ fi
 ###############################################################################
 # GET DEEZ ====================================================================
 if [[ "$INSTALL_DEEZ" -eq "1" ]]; then
+# sudo apt-get install libcurl4-nss-dev
+# sudo apt-get install libcurl-dev
 rm -rf deez
-git clone https://github.com/sfu-compbio/deez.git
+git clone https://bitbucket.org/compbio/dz.git
+mv dz deez/
 cd deez;
 make -j
-cp deez ../
+#cp deez ../
 cd ../
 fi
 ###############################################################################
@@ -321,7 +352,7 @@ git clone https://github.com/sfu-compbio/scalce.git
 cd scalce
 make download
 make
-cp scalce ../scalce-bin
+#cp scalce ../scalce-bin
 cd ../
 fi
 ###############################################################################
@@ -330,11 +361,13 @@ if [[ "$INSTALL_LWFQZIP" -eq "1" ]]; then
 rm -rf LWFQZip-v1.02.zip
 wget http://csse.szu.edu.cn/staff/zhuzx/LWFQZip/LWFQZip-v1.02.zip
 unzip LWFQZip-v1.02.zip
-cd LWFQZip-v1.02
+mv LWFQZip-v1.02 lwfqzip
+cd lwfqzip/
 make
 make clean;
 make
 cd ../
+rm -rf LWFQZip-v1.02.zip
 fi
 ###############################################################################
 # GET LEON ====================================================================
